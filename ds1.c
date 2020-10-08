8bd30d89af0fe68c2cd4e0a1ed81ebcb2fdc8dfe
sudhir33 <32663121+sudhir33@users.noreply.github.com>
2020-10-08 14:48:48 +0530

    Add files via upload

---
+++ ds1.c
@@ -0,0 +1,132 @@
+1.Stack follows
+	A) Last in first out
+	B) First in last out
+	C) Last in last out
+	D) First in first out
+
+2.Inserting an item into the stack when stack is not
+	full is called …………. Operation and deletion of item
+	form the stack, when stack is not empty is called ………..operation.
+	A) push, pop
+	B) pop, push
+	C) insert, delete
+	D) delete, insert
+
+3. ……………. Is a pile in which items are added at one
+	end and removed from the other.
+	A) Stack
+	B) Queue
+	C) List
+	D) None of the above
+	
+	
+4. ………… is very useful in situation when data have
+	to stored and then retrieved in reverse order.// 1 2 3 4
+	A) Stack
+	B) Queue
+	C) List
+	D) Link list
+	
+5. A ……. is a data structure that organizes data
+	similar to a line in the supermarket, where the first
+	one in line is the first one out.
+	A) Queue linked list
+	B) Stacks linked list
+	C) Both of them
+	D) Neither of them
+	
+6.Which of the following is non-liner data structure?
+	A) Stacks
+	B) List
+	C) Strings
+	D) Trees
+
+7. Which of the following data structure is non linear type?
+	A) Strings
+	B) Lists
+	C) Stacks
+	D) Graph
+8.Which of the following data structure is linear type?
+	A) Graph
+	B) Trees
+	C) Binary tree
+	D) Stack
+
+9. What will be the value of top, if there is a size of
+	stack STACK_SIZE is 5
+	A) 5
+	B) 6
+	C) 4
+	D) None
+// 0 1 2 3 4
+10.When new data are to be inserted into a data
+	structure, but there is not available space; this
+	situation is usually called ….
+	A. Underflow
+	B. overflow
+	C. houseful
+	D. saturated
+
+11.A data structure where elements can be added or
+	removed at either end but not in the middle is called …
+	A. linked lists//
+	B. stacks//
+	C. queues//
+	D. dequeue//
+	
+12.Each node in a linked list has two pairs of …………..and ……………….
+	A) Link field and information field
+	B) Link field and avail field
+	C) Avail field and information field
+	D) Address field and link field
+	
+13. When does top value of the stack changes?
+	A) Before deletion
+	B) While checking underflow
+	C) At the time of deletion
+	D) After deletion
+// 1 2 3top
+14.A linear list in which each node has pointers to
+	point to the predecessor and successors nodes is
+	called as ..
+	A) Singly Linked List
+	B) Circular Linked List
+	C) Doubly Linked List
+	D) Linear Linked List
+
+15. In linked representation of stack ……. holds the elements of the stack.
+	A. INFO fields
+	B. TOP fields
+	C. LINK fields
+	D. NULL fields
+16.  Deletion operation is done using ……… in a queue.
+	A. front
+	B. rear
+	C. top
+	D. list
+	
+17.Which is the pointer associated with the stack?
+	A. FIRST
+	B. FRONT
+	C. TOP
+	D. REAR
+18.The retrieval of items in a stack is ……….. operation.
+	A. push
+	B. pop
+	C. retrieval
+	D. access
+//1 2 3 4
+//4 3 2 1
+19.Which of the following is true about linked list implementation of stack?
+	A.In push operation, if new nodes are inserted at the beginning of linked list, 
+		then in pop operation, nodes must be removed from end.
+	B.In push operation, if new nodes are inserted at the end, 
+		then in pop operation, nodes must be removed from the beginning.
+	C. Both of the above
+	D. None of the above
+	
+	
+	
+	
+	
+	
